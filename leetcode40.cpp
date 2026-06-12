2d6dc080d33e55ec06490d7b72d2029b3942b59a
achalPatill <achalpatil1352@gmail.com>
2026-06-12 23:13:34 +0530

    que no 40 solved

---
+++ leetcode40.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 &&
+                   nums[i] <= n &&
+                   nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
+
+int main() {
+    vector<int> nums = {3, 4, -1, 1};
+
+    Solution obj;
+    cout << obj.firstMissingPositive(nums);
+
+    return 0;
+}
